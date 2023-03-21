e5003039e47cb426ab989de385aa46623f0476f0
Mikhail Shutov <mikhail.shutov@raiffeisen.ru>
2023-03-21 15:01:14 +0300

    add tests for account move

--- src/main/java/com/acme/banking/dbo/service/Reporting.java
+++ src/main/java/com/acme/banking/dbo/service/Reporting.java
@@ -2,7 +2,11 @@ package com.acme.banking.dbo.service;
 
 import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
+import com.acme.banking.dbo.domain.Client;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -14,6 +18,24 @@ public class Reporting {
     public String getReport(Branch rootBranch) {
         return "Branch #" + rootBranch.getId() + "\n==========\n" +
                 rootBranch.getAccounts().stream().collect(groupingBy(Account::getClient)).entrySet().stream()
-                        .map(entry -> "Client #" + entry.getKey().getId() + ": " + entry.getKey().getName() + "\n----------\n" + entry.getValue().stream().map(acc -> "- Account #" + acc.getId() + ": " + acc.getAmount()).collect(Collectors.joining("\n"))).collect(Collectors.joining("\n"));
+                        .sorted(Comparator.comparing(entry -> entry.getKey().getId()))
+                        .map(Reporting::printClientAndAccounts)
+                        .collect(Collectors.joining("\n\n"));
+    }
+
+    private static String printClientAndAccounts(Map.Entry<Client, List<Account>> entry) {
+        Client client = entry.getKey();
+        List<Account> accounts = entry.getValue();
+        return printClient(client)
+                + "\n----------\n"
+                + accounts.stream().map(Reporting::printAccount).collect(Collectors.joining("\n"));
+    }
+
+    private static String printAccount(Account acc) {
+        return "- Account #" + acc.getId() + ": " + acc.getAmount();
+    }
+
+    private static String printClient(Client client) {
+        return "Client #" + client.getId() + ": " + client.getName();
     }
 }
--- src/test/java/com/acme/banking/dbo/service/ReportingTest.java
+++ src/test/java/com/acme/banking/dbo/service/ReportingTest.java
@@ -3,22 +3,65 @@ package com.acme.banking.dbo.service;
 import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
 import com.acme.banking.dbo.domain.Client;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ReportingTest {
+    @Mock
+    Client stubClient;
+    @Mock
+    Client stubClient2;
+    @Mock
+    Account stubAccount;
+    @Mock
+    Account stubAccount2;
+    @Mock
+    Account stubAccount3;
+    @Mock
+    Branch stubBranch;
+
+    private List<Account> accounts = List.of();
+
+    @BeforeEach
+    public void setup() {
+        when(stubBranch.getId()).thenReturn(1);
+        when(stubClient.getId()).thenReturn(1);
+        when(stubClient.getName()).thenReturn("Name1");
+        when(stubClient2.getId()).thenReturn(2);
+        when(stubClient2.getName()).thenReturn("Name2");
+
+        accounts = new ArrayList<>();
+
+
+
+        when(stubAccount.getClient()).thenReturn(stubClient);
+        when(stubAccount.getId()).thenReturn(1);
+        when(stubAccount.getAmount()).thenReturn(100.0);
+        when(stubAccount2.getClient()).thenReturn(stubClient);
+        when(stubAccount2.getId()).thenReturn(2);
+        when(stubAccount2.getAmount()).thenReturn(200.0);
+
+        when(stubAccount3.getClient()).thenReturn(stubClient2);
+        when(stubAccount3.getId()).thenReturn(3);
+        when(stubAccount3.getAmount()).thenReturn(300.0);
+    }
+
     @Test
     public void shouldPrintBranchesWithoutAccounts() {
         Reporting sut = new Reporting();
 
-        Branch stubBranch = mock(Branch.class);
-        when(stubBranch.getId()).thenReturn(1);
+
         String result = sut.getReport(stubBranch);
 
         assertEquals("Branch #1", result);
@@ -26,16 +69,8 @@ class ReportingTest {
 
     @Test
     public void shouldPrintBranchesWithOneAccount() {
-        Branch stubBranch = mock(Branch.class);
-        Account stubAccount = mock(Account.class);
-        Client client = mock(Client.class);
-        when(client.getId()).thenReturn(1);
-        when(client.getName()).thenReturn("Name1");
-        when(stubAccount.getClient()).thenReturn(client);
-        when(stubAccount.getId()).thenReturn(1);
-        when(stubAccount.getAmount()).thenReturn(100.0);
+
         when(stubBranch.getAccounts()).thenReturn(List.of(stubAccount));
-        when(stubBranch.getId()).thenReturn(1);
         Reporting sut = new Reporting();
         assertEquals("""
                 Branch #1
@@ -43,5 +78,35 @@ class ReportingTest {
                 Client #1: Name1
                 ----------
                 - Account #1: 100.0""", sut.getReport(stubBranch));
+    }
+
+    @Test
+    public void shouldPrintBranchesWithTwoAccounts() {
+        when(stubBranch.getAccounts()).thenReturn(List.of(stubAccount, stubAccount2));
+        Reporting sut = new Reporting();
+        assertEquals("""
+                Branch #1
+                ==========
+                Client #1: Name1
+                ----------
+                - Account #1: 100.0
+                - Account #2: 200.0""", sut.getReport(stubBranch));
+    }
+
+    @Test
+    public void shouldPrintBranchesWithAccountsForTwoClients() {
+        when(stubBranch.getAccounts()).thenReturn(List.of(stubAccount, stubAccount2, stubAccount3));
+        Reporting sut = new Reporting();
+        assertEquals("""
+                Branch #1
+                ==========
+                Client #1: Name1
+                ----------
+                - Account #1: 100.0
+                - Account #2: 200.0
+                
+                Client #2: Name2
+                ----------
+                - Account #3: 300.0""", sut.getReport(stubBranch));
     }
 }
