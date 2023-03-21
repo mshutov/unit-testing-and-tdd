a2da9fdb0a2e5510d18f4ca91a6b173b939b9b7a
Mikhail Shutov <mikhail.shutov@raiffeisen.ru>
2023-03-21 14:36:46 +0300

    add tests for account move

--- src/main/java/com/acme/banking/dbo/service/Reporting.java
+++ src/main/java/com/acme/banking/dbo/service/Reporting.java
@@ -1,12 +1,19 @@
 package com.acme.banking.dbo.service;
 
+import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
+
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Reporting {
     /**
      * @return Markdown report for all branches, clients, accounts
      */
     public String getReport(Branch rootBranch) {
-        return "Branch #" + rootBranch.getId();
+        return "Branch #" + rootBranch.getId() + "\n==========\n" +
+                rootBranch.getAccounts().stream().collect(groupingBy(Account::getClient)).entrySet().stream()
+                        .map(entry -> "Client #" + entry.getKey().getId() + ": " + entry.getKey().getName() + "\n----------\n" + entry.getValue().stream().map(acc -> "- Account #" + acc.getId() + ": " + acc.getAmount()).collect(Collectors.joining("\n"))).collect(Collectors.joining("\n"));
     }
 }
--- src/test/java/com/acme/banking/dbo/service/ReportingTest.java
+++ src/test/java/com/acme/banking/dbo/service/ReportingTest.java
@@ -1,8 +1,12 @@
 package com.acme.banking.dbo.service;
 
+import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
+import com.acme.banking.dbo.domain.Client;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -23,13 +27,21 @@ class ReportingTest {
     @Test
     public void shouldPrintBranchesWithOneAccount() {
         Branch stubBranch = mock(Branch.class);
+        Account stubAccount = mock(Account.class);
+        Client client = mock(Client.class);
+        when(client.getId()).thenReturn(1);
+        when(client.getName()).thenReturn("Name1");
+        when(stubAccount.getClient()).thenReturn(client);
+        when(stubAccount.getId()).thenReturn(1);
+        when(stubAccount.getAmount()).thenReturn(100.0);
+        when(stubBranch.getAccounts()).thenReturn(List.of(stubAccount));
+        when(stubBranch.getId()).thenReturn(1);
         Reporting sut = new Reporting();
         assertEquals("""
                 Branch #1
-                    ==========
-                    Client #1: Name1
-                    ----------
-                    - Account #1: 100.0
-                """, sut.getReport(stubBranch));
+                ==========
+                Client #1: Name1
+                ----------
+                - Account #1: 100.0""", sut.getReport(stubBranch));
     }
 }
